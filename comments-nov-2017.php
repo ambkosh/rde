80e69ef8106a1f6c083f7bdec650b24a142c2404
ambkosh <ajcrizon@gmail.com>
2017-11-30 19:00:44 +0100

    First updated version with new stuff for comment table

--- comments-nov-2017.php
+++ comments-nov-2017.php
@@ -3,7 +3,14 @@
  include 'header.php';
 ?> 
 
-<p>Hello World part one!</p>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="script/tablesubmitter.js"></script>
+
+<div id="maincontent" class="container-fuid" style="line-height:1;">    
+    <table id="dtable" class="display table small">
+    </table>
+</div>
 
  </body>
 </html>
